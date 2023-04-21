9a35df8f4315105a56dac8e44bd449cf06928e30
Omari Warner <107162980+omari-war@users.noreply.github.com>
2023-04-21 00:43:34 -0400

    Update install.sh

--- Ubuntu/Main_Desktop/22.10/install.sh
+++ Ubuntu/Main_Desktop/22.10/install.sh
@@ -16,25 +16,30 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 7A3A762FAFD4A51F
 
 #download packages
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-wget https://dl.discordapp.net/apps/linux/0.0.25/discord-0.0.25.deb
+wget https://dl.discordapp.net/apps/linux/0.0.25/discord-0.0.26.deb
 wget -O- https://aka.ms/install-vscode-server/setup.sh | sh
 wget -O- https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
 #add apt repository
 sudo add-apt-repository ppa:nextcloud-devs/client
-sudo add-apt-repository ppa:otto-kesselgulasch/gimp
 sudo add-apt-repository "deb http://repository.spotify.com stable non-free"
 
 # Install packages and applications
 sudo apt install make libssl-dev libghc-zlib-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip -y
-sudo apt install -y libc++1 curl git cmake python3-pip openssh-server python3 docker.io docker-compose transmission g++ build-essential ascii 
-sudo apt install -y gimp nextcloud-client spotify-client inkspace timeshift
-echo finished this part
+sudo apt install -y libc++1 curl git cmake python3-pip openssh-server postman python3 jackd docker.io docker-compose transmission g++ build-essential ascii 
+sudo apt autoremove
+sudo apt install -y nextcloud-client spotify-client inkspace timeshift jackd
+
 #Run downloaded packages
 sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo dpkg -i discord-0.0.25.deb
+sudo dpkg -i discord-0.0.26.deb
 
 #start pakages
 sudo systemctl enable ssh
+
+#git config
+git config --global user.name "Omari Warner"
+git config --global user.email "omari.warner@outlook.com"
+
 
 # Example: sudo apt install curl firefox
 
